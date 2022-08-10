5a931840a323640d9926127a830281cb2cdd9c4d
Denver <den-knyazev@mail.ru>
2022-08-10 21:41:32 +0300

    Домашка ООП2

---
+++ src/Bicycle.java
@@ -0,0 +1,11 @@
+public class Bicycle extends WheelableTransport {
+
+
+    protected Bicycle(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}
---
+++ src/WheelableTransport.java
@@ -0,0 +1,26 @@
+public abstract class WheelableTransport {
+    private String modelName;
+    private int wheelsCount;
+
+
+    protected WheelableTransport(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+}
